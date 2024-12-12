1c6efde58cab9ce20807b905ba6613032faf714d
Halil Özcan <halil_ozcann@outlook.com>
2024-12-12 16:44:11 +0300

    C# Diziler Yapıldı

--- Diziler/Program.cs
+++ Diziler/Program.cs
@@ -10,6 +10,24 @@ namespace Diziler
     {
         static void Main(string[] args)
         {
+            string[] ogrenciListesi = new string[5];
+
+            ogrenciListesi[0] = "Halil";
+            ogrenciListesi[1] = "Hasan";
+            ogrenciListesi[2] = "Kemal";
+            ogrenciListesi[3] = "Hakan";
+            ogrenciListesi[4] = "Deniz";
+
+            foreach (var item in ogrenciListesi)
+            {
+                Console.WriteLine(item);
+            }
+
+            for(int i = 0; i < ogrenciListesi.Length; i++)
+            {
+                string gelenİsim = ogrenciListesi[i];
+                Console.WriteLine(gelenİsim);
+            }
         }
     }
 }
