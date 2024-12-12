25def7b99c286ac98acefa7eff1281939c532912
Halil Özcan <halil_ozcann@outlook.com>
2024-12-12 17:07:36 +0300

    C# Dizi Alistirma 1 Yapıldı

--- DizilerAlistirma1/Program.cs
+++ DizilerAlistirma1/Program.cs
@@ -10,6 +10,40 @@ namespace DizilerAlistirma1
     {
         static void Main(string[] args)
         {
+            // Uygulama1 : int veri tipinde bir dizi oluşturun. kullanici dizinini kaç elemanlı olduğunu belirlesin ve 
+            // dize içerisindeki alanlara değer ataması yapsın. daha sonra dizi içerisindeki elemanların toplamlarını ve ortalamasını yazdırsın.
+
+            Console.WriteLine("Lütfen oluşturmak istediğiniz veri kümesinin uzunluğunu Giriniz");
+            string uygulama1DiziUzunluk = Console.ReadLine();
+            int ugulamaDiziUzunlukint = int.Parse(uygulama1DiziUzunluk);
+
+            int[] uygulama1Dizi = new int[ugulamaDiziUzunlukint];
+
+            for(int uygulamaSayac = 0;  uygulamaSayac < uygulama1Dizi.Length; uygulamaSayac++)
+            {
+                Console.WriteLine("{0} . index e denk gelen degeri giriniz", uygulamaSayac);
+                uygulama1Dizi[uygulamaSayac] = int.Parse(Console.ReadLine());   
+            }
+
+            Console.WriteLine("Tebrikler dizinin içerisindeki tüm alanları doldurdunuz.");
+
+            Console.WriteLine("Dizi içerisindeki degerler ve toplam, ortalama degeri asağıdaki gibidir.");
+
+            int uygulamaToplam =0 , uygulamaOrtalama = 0;
+            foreach(int item in uygulama1Dizi)
+            {
+                Console.WriteLine(item);
+                uygulamaToplam = uygulamaToplam + item;
+            }
+
+
+            Console.WriteLine("---------------------------------------");
+
+            Console.WriteLine(uygulamaToplam);
+
+            Console.WriteLine("Dizi İçerisindeki eleman Sayısı : {0} ", uygulama1Dizi.Length);
+            uygulamaOrtalama = uygulamaToplam / uygulama1Dizi.Length;
+            Console.WriteLine("Ortalama : {0} ", uygulamaOrtalama);
         }
     }
 }
